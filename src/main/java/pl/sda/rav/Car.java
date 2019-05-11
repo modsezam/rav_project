560393b23083a7aa375006aef6ec7c57fdb0cfb0
Grzegorz <gcybula@onet.pl>
2019-05-11 11:35:02 +0200

    wersja 5

--- src/main/java/pl/sda/rav/Car.java
+++ src/main/java/pl/sda/rav/Car.java
@@ -3,5 +3,10 @@ package pl.sda.rav;
 public class Car extends Vehicels {
    private int maxDistanceKm;
    private String bodyType;
+
+
+   public Car(String vin, String status, String name, int productionDate, int maxDistanceKm, String bodyType) {
+      super(vin, status, name, productionDate);
+   }
 }
 
