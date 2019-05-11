23fdad4b3eec0974dd456ac44802829caec13049
marcin.kobierowski <modsezam@gmail.com>
2019-05-11 12:38:00 +0200

    period class and periodTest

--- src/main/java/pl/sda/rav/OdrerDao.java
+++ src/main/java/pl/sda/rav/OdrerDao.java
@@ -1,12 +1,23 @@
 package pl.sda.rav;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class OdrerDao {
 
     ArrayList<Order> orders = new ArrayList<>();
 
+    public boolean isAvailability(String vin, LocalDate startDate, LocalDate endTime){
+        boolean result = false;
 
+        for (Order order : orders) {
+            if (order.period.isAvailability(startDate, endTime) == true){
+                result = true;
+            }
+        }
+
+        return result;
+    }
 
 
 
