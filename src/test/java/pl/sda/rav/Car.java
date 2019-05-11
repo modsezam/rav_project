ed099ddff6823221a5c7dfe0b908e5d43b023f7c
Grzegorz <gcybula@onet.pl>
2019-05-11 10:58:38 +0200

    wersja 1

---
+++ src/test/java/pl/sda/rav/Car.java
@@ -0,0 +1,13 @@
+package pl.sda.rav;
+
+public class Car {
+   private String vin;
+   private String status;
+   private String name;
+   private int productionDate;
+   private int maxDistance;
+   private String bodyType;
+
+
+}
+
